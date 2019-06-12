be6352822e1c0b96e02d4263751b0961ccefdd0b
Anthony Rurup <tcrurup@gmail.com>
2019-06-12 20:07:51 +0000

    Automatically backed up by Learn

--- app/models/pirate.rb
+++ app/models/pirate.rb
@@ -1,2 +1,21 @@
 class Pirate
+  
+  attr_accessor :name, :weight, :height
+  
+  ALL_PIRATES = []
+  
+  def initialize(args)
+    @name = args[:name]
+    @weight = args[:weight]
+    @height = args[:height]
+    ALL_PIRATES << self
+  end
+  
+  def self.all
+    ALL_PIRATES
+  end
+  
+  def self.clear
+    ALL_PIRATES.clear
+  end
 end
